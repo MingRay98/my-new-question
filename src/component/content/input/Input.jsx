603b34faf56a99f652a5c0f598e29ac7eb0ac933
MingRay <mingray@gmail.com>
2020-03-18 18:49:19 +0800

    Change some bugs

--- src/component/content/input/Input.jsx
+++ src/component/content/input/Input.jsx
@@ -1,11 +1,14 @@
 import React, {Component, useState} from 'react';
 import {styles} from './styles';
 
+let name = ""
+
 const Input = (props) => {
-  const [name, setName] = useState('');
+  // const [name, setName] = useState('');
+
   return (
     <div style={styles.containters}>
-      <input type="text" placeholder="請輸入Your Name Plz" onChange={(e) => {setName(e.target.value)}} />
+      <input type="text" placeholder="請輸入Your Name Plz" onChange={(e) => {name = e.target.value}} />
       <button className='btn btn-dark' onClick={() => props.handleSubmit(name)}>提交</button>
     </div>
   )
